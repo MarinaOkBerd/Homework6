34654c34f13d7d17988d9ee4044fe616e6a36cca
Marina Berdnikova <berdni4ka@rambler.ru>
2022-12-22 02:46:52 +0300

    Add Homework 6

---
+++ C#_Homework_Task41/Program.cs
@@ -0,0 +1,18 @@
+﻿//  41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.WriteLine("Введите числа.");
+string numbers = Console.ReadLine()??"";
+int [] array = Array.ConvertAll(numbers.Split(), int.Parse);
+int counter = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        counter ++;
+    }
+    
+}
+
+Console.WriteLine($"Введено {counter} чисел больше нуля.");
+
